2d2e2f3065deb8c0b169e7ce4319f03779ad8a97
Илюша <79216023478@yandex.ru>
2021-03-30 18:45:06 +0300

    add new class RouteModel, move class dbManager to optianal file

--- main.py
+++ main.py
@@ -2,59 +2,18 @@ import sys
 import os.path
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtWidgets import QApplication
-import peewee
-from dbManager import dbManager
+
+from dbManager import dbManager, Routes, Points, UnknownField
+from MyModel import RouteModel
 
 from peewee import *
 from datetime import date
 
+from PyQt5.QtCore import QAbstractListModel
+
 
 #database = SqliteDatabase('resources.db')
 
-class UnknownField(object):
-    def __init__(self, *_, **__): pass
-
-class BaseModel(Model):
-    class Meta:
-        database = SqliteDatabase('resources.db')
-
-class Routes(BaseModel):
-    amount = IntegerField(null=True)
-    date_of_creation = DateField(null=True)
-    name = CharField(null=True)
-    route_id = AutoField()
-
-    class Meta:
-        table_name = 'routes'
-
-class Points(BaseModel):
-    latitude = FloatField(null=True)
-    longitude = FloatField(null=True)
-    number = IntegerField(null=True)
-    route = ForeignKeyField(column_name='route_id', field='route_id', model=Routes, null=True)
-
-    class Meta:
-        table_name = 'points'
-        primary_key = False
-
-class dbManager:
-    def __init__(self, db):
-        self.database = db
-        self.database.connect()
-
-    def add_route(self, r_id, n, d, am):
-        Routes.create(route_id = r_id, name = n, date_of_creation = d, amount = am)
-
-    def remove_route(self, r_id):
-        sql = "DELETE FROM routes WHERE route_id=?"
-        self.database.execute_sql(sql, (r_id,))
-
-    def add_point(self, num, lat, lon, r_id):
-        Points.create(number = num, latitude = lat, longitude = lon, route = r_id)
-
-    def remove_point(self, r_id, num):
-        sql = "DELETE FROM points WHERE route_id=? AND number=?"
-        self.database.execute_sql(sql, (r_id, num))
 
 def on_qml_mouse_clicked(x, y):
     print('mouse clicked:', x, y)
@@ -73,14 +32,15 @@ if __name__ == "__main__":
         exit()
     app = QApplication([])
     engine = QQmlApplicationEngine()
+
+    db = dbManager(SqliteDatabase('resources.db'))
+    model = RouteModel(db)
+    engine.rootContext().setContextProperty('routeModel', model)
+
     engine.load(qml_doc_path)
     qml_root = engine.rootObjects()[0]
+
     qml_root.clicked.connect(on_qml_mouse_clicked)
-    db = dbManager(SqliteDatabase('resources.db'))
-    print(type(db))
-#    db.init()
-#    db.add_route(3, 'new', date(2005, 7, 14), 3)
-#    db.remove_route(3)
-#    db.add_point(4, 4, 4, 1)
-    db.remove_point(1, 4)
+
     sys.exit(app.exec_())
+#    del db
